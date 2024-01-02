88af80b1a2ebbf678d343bba854a610a3c1df361
Mateusz Budzisz <electroluxv2@gmail.com>
2024-01-03 00:28:35 +0100

    P6 path change

--- P6 - OpenCV/src/get-image-segmenter.ts
+++ P6 - OpenCV/src/get-image-segmenter.ts
@@ -1,16 +1,17 @@
 import { ImageSegmenter } from '@mediapipe/tasks-vision';
 import wasmLoaderPath from '../node_modules/@mediapipe/tasks-vision/wasm/vision_wasm_internal.js?url';
 import wasmBinaryPath from '../node_modules/@mediapipe/tasks-vision/wasm/vision_wasm_internal.wasm?url';
+import modelAssetPath from '/selfie_multiclass_256x256.tflite?url';
 
 export const getImageSegmenter = async () => {
   const wasmFileset: Parameters<typeof ImageSegmenter.createFromOptions>[0] = {
-    wasmBinaryPath: wasmBinaryPath,
-    wasmLoaderPath: wasmLoaderPath
+    wasmBinaryPath,
+    wasmLoaderPath,
   };
 
   return await ImageSegmenter.createFromOptions(wasmFileset, {
     baseOptions: {
-      modelAssetPath: "/selfie_multiclass_256x256.tflite",
+      modelAssetPath,
       delegate: "GPU",
     },
     runningMode: "VIDEO",
